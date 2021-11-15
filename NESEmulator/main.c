57574ffd1d62eb26c5349fa1c53391d2397101be
Hugo Barroca <hugobarroca98@gmail.com>
2021-11-15 21:01:51 +0000

    Fixed infinite loop when entering non-integer characters.

--- NESEmulator/main.c
+++ NESEmulator/main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
-    int commandInteger;
-    for(;;){
-        printf("Welcome to NESEmulator! Please select an option:\n");
-        printf("1. Quit.\n");
-        scanf("%d", &commandInteger);
-        if(commandInteger == 1){
-            printf("Exiting program!\n");
-            return 0;
-        }else{
-            printf("Command not recognized.");
-        }
-    }
+int welcomeScreen(){	
+	int commandInteger;
+	char c;
 
-    return 0;
+	for(;;){
+		printf("Welcome to NESEmulator! Please select an option:\n");
+		printf("0. Quit.\n");
+		printf("1. Load game.\n");
+		c = getchar();
+		if(c == '0'){
+			printf("Exiting program!\n");
+			return 0;
+		}
+		if(c == '1'){
+			printf("This functionality has not yet been implemented.\n");
+			continue;
+		}
+		printf("Command not recognized.\n\n");
+	}
+
+	return 0;
 }
+
+int main(int argc, char *argv[]){
+	welcomeScreen();
+	return 0;
+}
+
