494ccf5753dddeeeb937ee1be3f33553075f967f
Hugo Barroca <hugobarroca98@gmail.com>
2024-08-11 18:06:29 +0100

    Added CPU header, and initialization function.

--- NESEmulator/cpu.c
+++ NESEmulator/cpu.c
@@ -1,14 +1,23 @@
 // This file is meant to hold all the necessary code to emulate the Ricoh 2A03 CPU (based on the 6502 CPU).
 #include <stdbool.h>
 #include "stack.h"
+#include "cpu.h"
 
-void runProcessor(){
-	int programCounter[2];
-	Stack s;
-	initStack(&s);
-
-	while(true){
-		//fetch instruction from memory
-		//execute instruction
+void initProcessor(CPU *cpu){
+	CPU cpu;
+	cpu.A = 0;
+	cpu.X = 0;
+	cpu.Y = 0;
+	cpu.P = 0;
+	cpu.S = 0;
+	cpu.PC = 0;
+	for(i = 0; i < 256; i++){
+		cpu.stack[i] = 0;
 	}
 }
+
+void executeInstruction(CPU *cpu){
+	//TODO
+	return -1;
+}
+
---
+++ NESEmulator/cpu.h
@@ -0,0 +1,9 @@
+typedef struct {
+	uint8_t A;
+	uint8_t X;
+	uint8_t Y;
+	uint8_t P;
+	uint8_t S;
+	uint16_t PC;
+	uint8_t stack[256];
+} CPU;
